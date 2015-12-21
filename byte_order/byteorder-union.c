06b772439f80970874237b7a2d6313b46e65f48c
JShadowMan <jshadowman@163.com>
2015-12-21 13:16:36 +0800

    tab truns to space:4

--- byte_order/byteorder-union.c
+++ byte_order/byteorder-union.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 union byteorder {
-	short apple;
-	char banana[sizeof(short)];
+    short apple;
+    char banana[sizeof(short)];
 };
 
 int main(void) {
-	union byteorder pear;
-	
-	pear.apple = 0x0102;
-	if (pear.banana[0] == 0x01) {
-		puts("Big-endian");
-	} else if (pear.banana[0] == 0x02) {
-		puts("Little-endian");
-	} else {
-		puts("UNKNOWN");
-	}
-	
-	return 0;
+    union byteorder pear;
+    
+    pear.apple = 0x0102;
+    if (pear.banana[0] == 0x01) {
+        puts("Big-endian");
+    } else if (pear.banana[0] == 0x02) {
+        puts("Little-endian");
+    } else {
+        puts("UNKNOWN");
+    }
+    
+    return 0;
 }
--- byte_order/byteorder.c
+++ byte_order/byteorder.c
@@ -4,16 +4,16 @@
 #include <netinet/in.h>
 
 int main(void) {
-	uint16_t apple = 0x0102;
-	
-//	printf("%s BytesOrde\n", CPU_VENDOR_OS);
-	if (*(char *)&apple == 0x01) {
-		puts("Big-endian");
-	} else if (*(char *)&apple == 0x02) {
-		puts("Little-endian");
-	} else {
-		puts("Unknown");
-	}
-	
-	return 0;
+    uint16_t apple = 0x0102;
+    
+//  printf("%s BytesOrde\n", CPU_VENDOR_OS);
+    if (*(char *)&apple == 0x01) {
+        puts("Big-endian");
+    } else if (*(char *)&apple == 0x02) {
+        puts("Little-endian");
+    } else {
+        puts("Unknown");
+    }
+    
+    return 0;
 }
