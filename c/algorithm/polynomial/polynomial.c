e6370b874fd1383f32209ce9c9644cdf1c7084b1
JShadowMan <jshadowman@163.com>
2016-03-08 02:53:07 +0800

    <fix>(c/algorithm/polynomial): improve and add multi
    improve -> addPolynomial
    add new -> multPolynomial
    
    module -> collectPolynomial

--- c/algorithm/polynomial/polynomial.c
+++ c/algorithm/polynomial/polynomial.c
@@ -14,6 +14,9 @@ void addNode(Poly *list, int coeffcient, int exponent);
 void printPolynomial(Poly *list);
 void addPolynomial(const Poly *poly1, const Poly *poly2, Poly *sum);
 void multPolynomial(const Poly *poly1, const Poly *poly2, Poly *sum);
+void collectPolynomial(Poly *poly);
+void deleteNode(Poly *list, Poly item);
+void destory(Poly *poly);
 
 int main(void) {
     Poly poly1, poly2, sum;
@@ -22,19 +25,26 @@ int main(void) {
     zeroPolynomial(&poly2);
     zeroPolynomial(&sum);
 
-    addNode(&poly1, 1, 2);
-    addNode(&poly1, 2, 4);
-    addNode(&poly1, 3, 9);
-    addNode(&poly1, 5, 9);
+    addNode(&poly1, 1, 3);
+    addNode(&poly1, 2, 5);
+    addNode(&poly1, 3, 10);
+    addNode(&poly1, 5, 11);
 
     addNode(&poly2, 1, 2);
-    addNode(&poly2, 2, 4);
+    addNode(&poly2, 2, 5);
     addNode(&poly2, 3, 9);
-    addNode(&poly2, 5, 9);
+    addNode(&poly2, 5, 11);
 
+    puts("AddPolynomial: ");
 
+    addPolynomial(&poly1, &poly2, &sum);
+    printPolynomial(&sum);
 
-    printPolynomial(&poly1);
+    puts("MulyPolunomial: ");
+
+    destory(&sum);
+    multPolynomial(&poly1, &poly2, &sum);
+    printPolynomial(&sum);
 
     return 0;
 }
@@ -57,20 +67,98 @@ void addNode(Poly *list, int coeffcient, int exponent) {
         el = (Poly)malloc(sizeof(Node));
         el->coeffcient = coeffcient;
         el->exponent   = exponent;
+        el->next       = NULL;
 
         for (curr = *list; curr->next != NULL; curr = curr->next);
         curr->next = el;
-
     }
 }
 
 void printPolynomial(Poly *list) {
-    for (Poly curr = *list; curr->next != NULL; curr = curr->next) {
-        printf("%d %d\n", curr->coeffcient, curr->exponent);
+    for (Poly curr = *list; curr != NULL; curr = curr->next) {
+        printf("%d*X^%d + ", curr->coeffcient, curr->exponent);
     }
+    putchar('\n');
 }
 
 void addPolynomial(const Poly *poly1, const Poly *poly2, Poly *sum) {
-    int index1 = 0;
-    int index2 = 0;
+    Poly index1 = *poly1;
+    Poly index2 = *poly2;
+
+    while ((index1 != NULL) && (index2 != NULL)) {
+        if (index1->exponent == index2->exponent) {
+            addNode(sum, index1->coeffcient + index2->coeffcient, index1->exponent);
+            
+            index1 = index1->next;
+            index2 = index2->next;
+        } else if (index1->exponent > index2->exponent) {
+            addNode(sum, index2->coeffcient, index2->exponent);
+            
+            index2 = index2->next;
+        } else {
+            addNode(sum, index1->coeffcient, index1->exponent);
+
+            index1 = index1->next;
+        }
+    }
+    if (index1 != NULL) {
+        for (; index1 != NULL; index1 = index1->next) {
+            addNode(sum, index1->coeffcient, index1->exponent);
+        }
+    }
+    if (index2 != NULL) {
+        for (; index2 != NULL; index2 = index2->next) {
+            addNode(sum, index2->coeffcient, index2->exponent);
+        }
+    }
+    collectPolynomial(sum);
 }
+
+void multPolynomial(const Poly *poly1, const Poly *poly2, Poly *sum) {
+    for (Poly line = *poly1; line != NULL; line = line->next) {
+        for (Poly row = *poly2; row != NULL; row = row->next) {
+            addNode(sum, (line->coeffcient) * (row->coeffcient), (line->exponent) + (row->exponent));
+        }
+    }
+    collectPolynomial(sum);
+}
+
+void collectPolynomial(Poly *poly) {
+    for (Poly current = *poly; current->next != NULL; current = current->next) {
+        for (Poly child = current->next; child != NULL;) {
+            if (child->exponent == current->exponent) {
+                current->coeffcient += child->coeffcient;
+                deleteNode(poly, child);
+            } else {
+                child = child->next;
+            }
+        }
+    }
+}
+
+void deleteNode(Poly *list, Poly item) {
+    for (Poly *current = list; *current;) {
+        Poly entry = *current;
+
+        if (entry == item) {
+            *current = entry->next;
+            free(entry);
+        } else {
+            current = &(entry->next);
+        }
+    }
+}
+
+void destory(Poly *poly) {
+    Poly current = *poly;
+    Poly temp;
+
+    while (current->next != NULL) {
+        temp = current->next;
+        free(current);
+        current = temp;
+    }
+    free(current);
+    zeroPolynomial(poly); 
+}
+
