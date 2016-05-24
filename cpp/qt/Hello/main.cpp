d493c2f1e19c54ecabb68e3e598adc9ac8779700
Wang <jshadowman@163.com>
2016-05-24 21:40:18 +0800

    Update main.cpp

--- cpp/qt/Hello/main.cpp
+++ cpp/qt/Hello/main.cpp
@@ -1,18 +1,11 @@
 #include <QApplication>
-#include <QTextCodec>
-#include <QWidget>
 #include <QLabel>
 
-int main(int argc, char * argv[]) {
-    QApplication app(argc, argv);
-    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
-    QWidget * widget = new QWidget;
-    QLabel label(widget);
+int main(int argc, char *argv[])
+{
+    QApplication window(argc, argv);
+    QLabel *label = new QLabel("Hello World!");
 
-    label.setText(QObject::tr("453123"));
-    widget->setMinimumWidth(640);
-    widget->setMinimumHeight(480);
-
-    widget->show();
-    return app.exec();
+    label->show();
+    return window.exec();
 }
