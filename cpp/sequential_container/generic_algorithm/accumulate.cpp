e40cd2ff1afadb2d69d2d737c17ed4731c0e7d02
JShadowMan <jshadowman@163.com>
2016-08-22 23:01:58 +0800

    <feat>(cpp): generic algorithm, accumulate

---
+++ cpp/sequential_container/generic_algorithm/accumulate.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <numeric>
+
+using namespace std;
+
+int main(void) {
+	std::vector<int> apple{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+	std::list<std::string> pear{ "This", " ", "is", " ", "a", " ", "string" };
+
+	std::cout << accumulate(apple.cbegin(), apple.cend(), 0) << std::endl;
+	std::cout << accumulate(pear.cbegin(), pear.cend(), string("")) << std::endl;
+
+	return 0;
+}
