6653bdb706c5206a75c2808c52450806c5ebb992
JShadowMan <jshadowman@163.com>
2016-09-21 20:40:41 +0800

    cpp update

--- cpp/sequential_container/generic_algorithm/lambda_capture.cpp
+++ cpp/sequential_container/generic_algorithm/lambda_capture.cpp
@@ -5,11 +5,15 @@ int main(void) {
 
     auto lambda_capture_value = [apple] () { return apple; };
     auto lambda_capture_reference = [&apple] () { return apple; };
+    auto lambda_auto_capture_value = [=] () { return apple; }
+    auto lambda_auto_capture_reference = [&] () { return apple; }
     apple = 0;
 
     std::cout << "apple = " << apple
         << ", lambda_capture_value return value = " << lambda_capture_value()
         << ", lambda_capture_reference return value = " << lambda_capture_reference()
+        << ", lambda_auto_capture_value return value = " << lambda_auto_capture_value()
+        << ", lambda_auto_capture_reference return value = " << lambda_auto_capture_reference()
         << std::endl;
 
     return 0;
