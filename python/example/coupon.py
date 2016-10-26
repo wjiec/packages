f6ec00ff4db18322fb0bb6be6e780a64d692cd0a
JShadowMan <jshadowman@163.com>
2016-10-26 20:59:52 +0800

    update coupon

--- python/example/coupon.py
+++ python/example/coupon.py
@@ -4,20 +4,13 @@ import random
 
 def conpon(length, count):
     result = []
+    charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
     for i in range(abs(count)):
         cp = ''
         for l in range(abs(length)):
-            t = random.randint(0, 2)
             ch = ''
-            if t == 0: # number
-                while ch in cp:
-                    ch = chr(random.randint(48, 57))
-            elif t == 1:
-                while ch in cp:
-                    ch = chr(random.randint(65, 90))
-            else:
-                while ch in cp:
-                    ch = chr(random.randint(97, 122))
+            while ch in cp:
+                ch = random.choice(charset)
             cp += ch
         result.append(cp)
 
