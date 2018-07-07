d638556285fe89084a628ff7572ad140a7323572
JShadowMan <shadowman@shellboot.com>
2018-07-07 15:44:51 +0800

    reconnect interface

--- openwrt/iface_reconnect.sh
+++ openwrt/iface_reconnect.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-env -i /sbin/ifup $1 >/dev/null 2>/dev/null
+I_IFACE=$1
+ALL_IFACE=`ubus list | grep network.interface. | awk -F"." '{print $3}'`
+
+EXISTS=`echo "${ALL_IFACE}" | grep "${I_IFACE}"`
+if [[ "${EXISTS}" == "${I_IFACE}" ]]; then
+    env -i /sbin/ifup "${I_IFACE}" >/dev/null 2>/dev/null
+    echo "${I_IFACE} reconnected."
+else
+    echo "${I_IFACE} not exists"
+fi
