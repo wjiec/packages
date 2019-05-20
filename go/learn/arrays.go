332c424a6fd128bb57fbd114494927635d805fd5
Jayson <shadowman@shellboot.com>
2019-05-20 23:37:07 +0800

    array and slice

---
+++ go/learn/arrays.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func passByValue(arr [5]int) {
+	for idx, val := range arr {
+		idx = idx * idx
+		val = val * val
+	}
+
+	// not effect
+	for idx, val := range arr {
+		fmt.Println(idx, val)
+	}
+}
+
+func main() {
+	var array1 [8]int
+	fmt.Println(array1)
+
+	array2 := [3]int { 1, 2, 3 }
+	fmt.Println(array2)
+
+	array3 := [...]int { 2, 4, 6, 8, 10 }
+	fmt.Println(array3)
+
+	array4 := [...][3]int {{0, 1, 2}, {0, 1, 2}}
+	fmt.Println(array4)
+
+	// bad
+	for i := 0; i < len(array2); i++ {
+		fmt.Println(i)
+	}
+
+	// good
+	for index, row := range array4 {
+		fmt.Println(index, row)
+	}
+
+	// test, pass by value
+	passByValue(array3)
+	for k, v := range array3 {
+		fmt.Println(k, v)
+	}
+
+
+}
---
+++ go/learn/slice.go
@@ -0,0 +1,86 @@
+package main
+
+import "fmt"
+
+func passByView(arr []int)  {
+	arr[0] = 0xff
+}
+
+func main() {
+	// test failure
+	//autoArray := [1024]int { iota }
+	arr1 := [...]int { 0, 1, 2, 3, 4, 5, 6, 7 }
+	fmt.Println(arr1[2:4]) // [2, 4)
+	fmt.Println(arr1[:4]) // [2, 4)
+	fmt.Println(arr1[2:]) // [2, 4)
+	fmt.Println(arr1[:]) // [2, 4)
+
+	s1 := arr1[:]
+	passByView(s1)
+	fmt.Println(s1)
+
+	s2 := arr1[2:4]
+	passByView(s2)
+	fmt.Println(s2)
+	fmt.Println("arr1 =", arr1)
+
+	// re-slice
+	fmt.Println(s2[1:])
+
+	// out of range
+	//fmt.Println(arr1[4:3]) // error
+
+	fmt.Println("arr1 =", arr1)
+	slice1 := arr1[2:4]
+	fmt.Println("slice1 =", slice1) // { arr1[2], arr1[3], [arr1[4]], [...] }
+	func() {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println(">>> Error:", r)
+			}
+		}()
+
+		for i := 0; i < 10; i++ {
+			fmt.Println(slice1[:i])
+		}
+	}()
+
+	fmt.Println("arr1 =", arr1)
+	fmt.Printf("slice1 = %v, len(slice1) = %d, cap(slice1) = %d\n", slice1, len(slice1), cap(slice1))
+
+	// append slice
+	slice2 := arr1[3:7]
+	fmt.Println("arr1 =", arr1)
+	fmt.Printf("slice2 = %v, len(slice2) = %d, cap(slice2) = %d\n", slice2, len(slice2), cap(slice2))
+	append1 := append(slice2, -1)
+	append2 := append(append1, -2)
+	append3 := append(append2, -3)
+	fmt.Printf("append1 = %v, len(append1) = %d, cap(append1) = %d\n", append1, len(append1), cap(append1))
+	fmt.Printf("append2 = %v, len(append2) = %d, cap(append2) = %d\n", append2, len(append2), cap(append2))
+	fmt.Printf("append3 = %v, len(append3) = %d, cap(append3) = %d\n", append3, len(append3), cap(append3))
+	fmt.Printf("arr1 = %v, len(arr1) = %d, cap(arr1) = %d\n", arr1, len(arr1), cap(arr1))
+
+	// directly definition slice
+	var slice3 []int
+	fmt.Println(slice3, slice3 == nil)
+
+	// append to slice
+	for i:= 0; i < 100; i++ {
+		slice3 = append(slice3, i)
+		fmt.Printf("len(slice3) = %d, cap(slice3) = %d\n", len(slice3), cap(slice3))
+	}
+
+	// make slice
+	slice4 := make([]int, 16)
+	slice5 := make([]int, 16, 64)
+	fmt.Printf("slice4 = %v, len(slice4) = %d, cap(slice4) = %d\n", slice4, len(slice4), cap(slice4))
+	fmt.Printf("slice5 = %v, len(slice5) = %d, cap(slice5) = %d\n", slice5, len(slice5), cap(slice5))
+
+	// copy
+	fmt.Println(copy(slice4, slice2))
+	fmt.Printf("slice4 = %v, len(slice4) = %d, cap(slice4) = %d\n", slice4, len(slice4), cap(slice4))
+
+	// delete
+	slice4 = append(slice4[:3], slice4[4:]...)
+	fmt.Printf("slice4 = %v, len(slice4) = %d, cap(slice4) = %d\n", slice4, len(slice4), cap(slice4))
+}
