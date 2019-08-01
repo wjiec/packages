7ab9012caea9bfed01e6e0f54b55f64e8549dc1a
Jayson <shadowman@shellboot.com>
2019-08-01 22:21:25 +0800

    endless by buffered

---
+++ go/learn/concurrent/channel/endless_2.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func work(in chan int, out chan int) {
+	for {
+		fmt.Println(1)
+		v := <-in
+		fmt.Println(2)
+		fmt.Println(v)
+		fmt.Println(3)
+		out <- v * v
+		fmt.Println(4)
+	}
+}
+
+func provide(in chan int)  {
+	for i := 0; i >= 0; i++ {
+		in <- i
+	}
+}
+
+func main() {
+	in := make(chan int, 10)
+	out := make(chan int, 10)
+
+	go work(in, out)
+	go provide(in)
+	time.Sleep(time.Hour)
+}
