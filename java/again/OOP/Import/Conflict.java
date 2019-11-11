30f7154943b6167d6dda851a072a1c51b634de33
Jayson <shadowman@shellboot.com>
2019-11-11 23:14:52 +0800

    package and import

---
+++ java/again/OOP/Import/Conflict.java
@@ -0,0 +1,15 @@
+package OOP.Import;
+
+import java.util.*;
+import java.sql.*;
+// special import
+import java.util.Date;
+
+public class Conflict {
+
+    public static void main(String[] args) {
+        Date utilDate = new Date();
+        java.sql.Date sqlDate = new java.sql.Date(0L);
+    }
+
+}
---
+++ java/again/OOP/Import/StaticImport.java
@@ -0,0 +1,11 @@
+package OOP.Import;
+
+import static java.lang.Math.*;
+
+public class StaticImport {
+
+    public static void main(String[] args) {
+        System.out.println(sqrt(pow(3, 2) + pow(4, 2)));
+    }
+
+}
