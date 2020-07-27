4573c0ad5185857e4ca89217dea6bdf5bc33cf61
Jayson <shadowman@shellboot.com>
2020-07-27 23:41:18 +0800

    lock and abstractqueuedsynchronizer

---
+++ java/concurrency-arts/src/main/java/com/wjiec/learn/lock/Mutex.java
@@ -0,0 +1,74 @@
+package com.wjiec.learn.lock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class Mutex implements Lock {
+
+    private final MutexSync sync = new MutexSync();
+
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long l, TimeUnit timeUnit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, timeUnit.toNanos(l));
+    }
+
+    @Override
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
+    static class MutexSync extends AbstractQueuedSynchronizer {
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+
+        @Override
+        protected boolean tryAcquire(int arg) {
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            if (getState() == 0) {
+                throw new IllegalMonitorStateException();
+            }
+
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        Condition newCondition() {
+            return new ConditionObject();
+        }
+
+    }
+}
