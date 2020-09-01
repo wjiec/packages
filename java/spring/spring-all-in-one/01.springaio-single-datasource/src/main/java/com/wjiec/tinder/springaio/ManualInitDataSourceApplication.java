7de55267fad1b38d5e62299e5d491489322b0ce2
Jayson <shadowman@shellboot.com>
2020-09-01 21:09:50 +0800

    spring all in one

---
+++ java/spring/spring-all-in-one/01.springaio-single-datasource/src/main/java/com/wjiec/tinder/springaio/ManualInitDataSourceApplication.java
@@ -0,0 +1,11 @@
+package com.wjiec.tinder.springaio;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ManualInitDataSourceApplication {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+    }
+
+}
---
+++ java/spring/spring-all-in-one/01.springaio-single-datasource/src/main/resources/database/h2/data.sql
@@ -0,0 +1,2 @@
+insert into `spring_user`(`id`, `username`, `password`)
+    values (1, 'apple', 'apple'), (2, 'banana', 'banana'), (3, 'cherry', 'cherry');
