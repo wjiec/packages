0ec4b61dd51c0b038871e452b75ac48cc7a54c5d
Jayson <shadowman@shellboot.com>
2020-09-16 21:51:21 +0800

    spring web

---
+++ java/spring/springinaction/src/main/java/com/wjiec/tinder/springinaction/spittr/config/RootConfig.java
@@ -0,0 +1,9 @@
+package com.wjiec.tinder.springinaction.spittr.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class RootConfig {
+}
---
+++ java/spring/springinaction/src/main/java/com/wjiec/tinder/springinaction/spittr/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.wjiec.tinder.springinaction.spittr.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+}
