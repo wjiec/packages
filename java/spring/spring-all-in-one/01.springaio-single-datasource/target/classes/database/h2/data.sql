49706d8ce99e46d2584cc478128ee011fe63d372
Jayson <shadowman@shellboot.com>
2020-12-09 23:52:23 +0800

    spring all in one

--- java/spring/spring-all-in-one/01.springaio-single-datasource/target/classes/database/h2/data.sql
+++
@@ -1,2 +0,0 @@
-insert into `spring_user`(`id`, `username`, `password`)
-    values (1, 'apple', 'apple'), (2, 'banana', 'banana'), (3, 'cherry', 'cherry');
--- java/spring/spring-all-in-one/01.springaio-single-datasource/target/classes/database/h2/schema.sql
+++
@@ -1,5 +0,0 @@
-create table `spring_user` (
-    `id` int identity,
-    `username` varchar not null default '',
-    `password` varchar not null default ''
-);
