3485bb824fed8aee0432e3b0b74098e831b07f7a
Jayson <shadowman@shellboot.com>
2020-12-24 00:10:18 +0800

    remove conditions

--- java/spring/spring-all-in-one/14-spring-cache/src/main/java/com/wjiec/springaio/cache/service/TimeService.java
+++ java/spring/spring-all-in-one/14-spring-cache/src/main/java/com/wjiec/springaio/cache/service/TimeService.java
@@ -3,6 +3,7 @@ package com.wjiec.springaio.cache.service;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -16,10 +17,14 @@ public class TimeService {
         return LocalDateTime.now();
     }
 
-    @CachePut(value = "cacheName", key = "#result.year", unless = "#result.second == 0", condition = "#result.second == 59")
+//    @CachePut(value = "cacheName", key = "#result.year", unless = "#result.second == 0", condition = "#result.second == 59")
+    @CachePut(value = "cacheName", key = "#result.year")
     public LocalDateTime update() {
         System.out.println("call update");
-        return LocalDateTime.now();
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println("Sec: " + localDateTime.getSecond());
+
+        return localDateTime;
     }
 
     @CacheEvict(value = "cacheName", key = "2020")
