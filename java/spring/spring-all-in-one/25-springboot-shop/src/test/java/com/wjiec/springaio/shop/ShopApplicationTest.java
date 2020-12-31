5d647df32acfd90b1262926896a85b0928b81b83
Jayson <shadowman@shellboot.com>
2020-12-31 22:09:54 +0800

    springboot shop

---
+++ java/spring/spring-all-in-one/25-springboot-shop/src/test/java/com/wjiec/springaio/shop/ShopApplicationTest.java
@@ -0,0 +1,12 @@
+package com.wjiec.springaio.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTest {
+
+    @Test
+    void contextLoaded() {}
+
+}
---
+++ java/spring/spring-all-in-one/25-springboot-shop/src/test/java/com/wjiec/springaio/shop/controller/IndexControllerTest.java
@@ -0,0 +1,26 @@
+package com.wjiec.springaio.shop.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+class IndexControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("index/home"))
+            .andExpect(content().string(containsString("Welcome to Spring Shop")));
+    }
+
+}
