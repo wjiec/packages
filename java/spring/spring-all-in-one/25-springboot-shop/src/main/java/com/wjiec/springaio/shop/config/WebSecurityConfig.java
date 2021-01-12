7bf67bc831c4a673cf91c67e40a34fac01fab29e
Jayson <shadowman@shellboot.com>
2021-01-12 23:29:08 +0800

    inMemory security config

---
+++ java/spring/spring-all-in-one/25-springboot-shop/src/main/java/com/wjiec/springaio/shop/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.wjiec.springaio.shop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("jayson").password("{noop}jayson").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}admin").roles("USER", "ADMIN");
+    }
+
+}
