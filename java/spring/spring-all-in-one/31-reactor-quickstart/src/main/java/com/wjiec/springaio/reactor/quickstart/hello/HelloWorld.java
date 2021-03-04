0fc4836f1ff9238f9aa7cf142f4d0cb92d36272f
Jayson <shadowman@shellboot.com>
2021-03-04 23:31:55 +0800

    reactor quickstart

---
+++ java/spring/spring-all-in-one/31-reactor-quickstart/src/main/java/com/wjiec/springaio/reactor/quickstart/hello/HelloWorld.java
@@ -0,0 +1,28 @@
+package com.wjiec.springaio.reactor.quickstart.hello;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+
+@Slf4j
+@Component
+public class HelloWorld implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) {
+        Mono.just(args[0])
+            .map(String::toUpperCase)
+            .map(s -> "Hello " + s)
+            .subscribe(log::info);
+
+        Flux.just(args)
+            .map(String::toCharArray)
+            .map(cs -> String.format("%s -> %d", Arrays.toString(cs), cs.length))
+            .subscribe(log::info);
+    }
+
+}
