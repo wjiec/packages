cf9a39cb47160666300cf0bee1fbb2ac620f15a5
Jayson <shadowman@shellboot.com>
2021-03-15 23:02:48 +0800

    add service and handle distinct instance-id

--- java/spring/spring-all-in-one/33-springcloud-item-service/src/main/java/com/wjiec/springaio/cloud/item/ItemServiceApplication.java
+++ java/spring/spring-all-in-one/33-springcloud-item-service/src/main/java/com/wjiec/springaio/cloud/item/ItemServiceApplication.java
@@ -2,8 +2,10 @@ package com.wjiec.springaio.cloud.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ItemServiceApplication {
 
     public static void main(String[] args) {
---
+++ java/spring/spring-all-in-one/33-springcloud-item-service/src/main/java/com/wjiec/springaio/cloud/item/controller/ItemController.java
@@ -0,0 +1,17 @@
+package com.wjiec.springaio.cloud.item.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/item")
+public class ItemController {
+
+    @GetMapping("/{name}")
+    public String item(@PathVariable String name) {
+        return name;
+    }
+
+}
