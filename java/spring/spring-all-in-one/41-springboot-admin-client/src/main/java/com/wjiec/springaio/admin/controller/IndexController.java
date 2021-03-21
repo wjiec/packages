504a64dafe0d764aec3236a4ce2cd8b8a3f81251
Jayson <shadowman@shellboot.com>
2021-03-21 17:07:22 +0800

    springboot admin server/client

---
+++ java/spring/spring-all-in-one/41-springboot-admin-client/src/main/java/com/wjiec/springaio/admin/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.wjiec.springaio.admin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String status() {
+        return "OK";
+    }
+
+}
