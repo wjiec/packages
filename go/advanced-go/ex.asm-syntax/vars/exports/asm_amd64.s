f141a27fb965ffaf1adcc8eead4fc793a82d1212
Jayson <shadowman@shellboot.com>
2021-04-27 23:52:34 +0800

    goasm vars

---
+++ go/advanced-go/ex.asm-syntax/vars/exports/asm_amd64.s
@@ -0,0 +1,46 @@
+
+// var ArrayValue [4]uint32
+GLOBL ·ArrayValue(SB), $32
+DATA ·ArrayValue+0(SB)/4, $0 // ArrayValue[0] = 0
+DATA ·ArrayValue+4(SB)/4, $1 // ArrayValue[1] = 1
+DATA ·ArrayValue+8(SB)/4, $2 // ArrayValue[2] = 2
+DATA ·ArrayValue+12(SB)/4, $3 // ArrayValue[3] = 3
+
+// var BoolValue bool
+GLOBL ·BoolValue(SB), $1 // uninitial
+
+// var TrueValue bool
+GLOBL ·TrueValue(SB), $1
+DATA ·TrueValue(SB)/1, $-1
+
+// var FalseValue bool
+GLOBL ·FalseValue(SB), $1
+DATA ·FalseValue(SB)/1, $0
+
+// var IntValue int
+GLOBL ·IntValue(SB), $8
+DATA ·IntValue(SB)/8, $0xffffffffffffffff
+
+// var Float32Value float32
+GLOBL ·Float32Value(SB), $4
+DATA ·Float32Value(SB)/4, $0x12345678
+
+// var Float64Value float32
+GLOBL ·Float64Value(SB), $8
+DATA ·Float64Value(SB)/8, $0x12345678
+
+// var StringValue string
+GLOBL ·StringValue(SB), $16
+DATA ·StringValue+0(SB)/8, $·text<>(SB)
+DATA ·StringValue+8(SB)/8, $5
+
+// var SliceValue string
+GLOBL ·SliceValue(SB), $24
+DATA ·SliceValue+0(SB)/8, $·text<>(SB)
+DATA ·SliceValue+8(SB)/8, $8
+DATA ·SliceValue+16(SB)/8, $16
+
+// internal
+GLOBL ·text<>(SB), NOPTR, $16
+//DATA ·text<>(SB)/8, $"hello world long long" // overflow // asm: WriteString: bad string size: 8 < 21
+DATA ·text<>(SB)/8, $"hello"
---
+++ go/advanced-go/ex.asm-syntax/vars/exports/exports.go
@@ -0,0 +1,15 @@
+package exports
+
+var ArrayValue [4]int32
+
+var BoolValue bool
+var TrueValue bool
+var FalseValue bool
+
+var IntValue int
+
+var Float32Value float32
+var Float64Value float64
+
+var StringValue string
+var SliceValue []byte
